9570a310e781fb73e964cb87381744f25e5e1683
MGatner <mgatner@icloud.com>
2020-09-27 21:35:36 +0000

    Bootstrap WP with wp-cli

--- tests/_support/bootstrap.php
+++ tests/_support/bootstrap.php
@@ -1,23 +1,40 @@
 <?php
-
-define('ABSPATH', HOMEPATH . 'vendor/johnpbloch/wordpress-core/');
-
-// Database configuration
-define('DB_NAME', 'ci4_tests');
-define('DB_USER', 'ci4_tests');
-define('DB_PASSWORD', 'aGlradmIzF@SGkW7n8UYCvEsAeADUaEk');
-define('DB_HOST', 'tatter.citsvlxipct1.us-east-1.rds.amazonaws.com');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
-
-require_once ABSPATH . 'wp-includes' . DIRECTORY_SEPARATOR . 'wp-db.php';
-
-$wpdb = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
-
-require_once ABSPATH . 'wp-admin' . DIRECTORY_SEPARATOR . 'wp-includes' . DIRECTORY_SEPARATOR . 'schema.php';
-
-$result = wp_get_db_schema();
-
+		
+// Bootstrap CodeIgniter
 require_once HOMEPATH . 'vendor/codeigniter4/codeigniter4/system/Test/bootstrap.php';
 
-var_dump($result);
+// Locate the installation directory
+defined('WORDPRESSPATH') || define('WORDPRESSPATH', HOMEPATH . 'build/wordpress/');
+
+// Download the latest WordPress
+$script = escapeshellcmd(HOMEPATH . 'vendor/bin/wp core download' .
+	' --path=' . WORDPRESSPATH .
+	' --skip-content' .
+	' --force'
+);
+echo $script . PHP_EOL;
+passthru($script);
+
+// Create the config file
+$script = escapeshellcmd(HOMEPATH . 'vendor/bin/wp config create' .
+	' --path=' . WORDPRESSPATH .
+	' --dbname=' . $_ENV['DB_NAME'] .
+	' --dbuser=' . $_ENV['DB_USER'] .
+	' --dbhost=' . $_ENV['DB_HOST'] .
+	' --dbpass=' . escapeshellarg($_ENV['DB_PASS']) .
+	' --force'
+);
+passthru($script);
+
+// Install WordPress
+$script = escapeshellcmd(HOMEPATH . 'vendor/bin/wp core install' .
+	' --path=' . WORDPRESSPATH .
+	' --url=example.com' .
+	' --title=Example' .
+	' --admin_user=supervisor' .
+	' --admin_password=strongpassword' .
+	' --admin_email=info@example.com' .
+	' --skip-email'
+);
+echo $script . PHP_EOL;
+passthru($script);
